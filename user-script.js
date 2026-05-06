f0615bd67e2302b74748b3fc4111b4dd033e8fdc
sevenRevy <76248552+sevenRevy@users.noreply.github.com>
2026-05-06 12:50:29 -0500

    Update user-script.js

--- user-script.js
+++ user-script.js
@@ -7,7 +7,7 @@
 // @match        https://gemini.google.com/*
 // @match        https://business.gemini.google/*
 // @grant        none
-// @license       GPL-3.0-only
+// @license      GPL-3.0-only
 // @icon         https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Google-gemini-icon.svg/960px-Google-gemini-icon.svg.png
 // ==/UserScript==
 
